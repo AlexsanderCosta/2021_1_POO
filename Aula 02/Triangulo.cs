134b23df81076f79005a9b7d7d826ce6131e83c2
AlexsanderCosta <49567620+AlexsanderCosta@users.noreply.github.com>
2021-01-16 20:55:10 -0300

    Create Triangulo.cs

---
+++ Aula 02/Triangulo.cs
@@ -0,0 +1,13 @@
+sing System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Digite a base e altura do retêngulo:");
+    double b = double.Parse(Console.ReadLine());
+    double a = double.Parse(Console.ReadLine());
+    double area = b * a;
+    double perímetro = b * 2 + a * 2;
+    double diagonal = Math.Sqrt((b * b + a * a)); 
+    Console.WriteLine($"Área = {area:0.00}  Perímetro = {perímetro:0.00} Diagonal = {diagonal:0.00}");
+  }
+}
